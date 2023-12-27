8f4665087ec02f81828e796dcf0ff2772d0e90a2
k03mad <k03.mad@gmail.com>
2023-12-28 01:11:32 +0300

    fix name

--- app/api/_index.js
+++ app/api/_index.js
@@ -2,4 +2,4 @@ export {default as CloudPing} from './cloud-ping.js';
 export {default as IPLeak} from './ip-leak.js';
 export {default as IPWhois} from './ip-whois.js';
 export {default as NextDNS} from './next-dns.js';
-export {default as Wander} from './wander1.js';
+export {default as Wander} from './wander.js';
